a60dda5328b952cb79069c0d81e04782815937dd
alfredanderschs <alfredanderschs@tuta.io>
2025-07-16 16:29:17 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Play Pogo'
+project = 'Official Page'
 copyright = '2025'
-author = 'Willi Heinrich'
+author = 'Wilcin'
 
 release = '1.0'
 
